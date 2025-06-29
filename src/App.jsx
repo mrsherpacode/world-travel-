566e73d2ac42c75aa163e140bfee604aa06f7728
mrsherpacode <sherpacoding@gmail.com>
2025-06-29 15:22:20 -0500

    i have added some nested routes inside AppLayout component

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,15 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/product" element={<Product />} />
         <Route path="/pricing" element={<Pricing />} />
-        <Route path="/app" element={<AppLayout />} />
+
+        <Route path="/app" element={<AppLayout />}>
+          {/* This is called index route which is render as default rout inside app route */}
+          <Route index element={<p>cities</p>} />
+          {/* Here, i'm creating nexted routes or child routes of app  */}
+          <Route path="cities" element={<p>The list of cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,15 @@
 import styles from "./Sidebar.module.css";
 import Logo from "./Logo";
 import AppNav from "./AppNav";
+import { Outlet } from "react-router-dom";
 //  sidebar component
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p> The list of Cities</p>
+      {/* The outlet component is a React Router component that serves as a placeholder for nested/child routes content */}
+      <Outlet />
       <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} by World-Travel INC.
