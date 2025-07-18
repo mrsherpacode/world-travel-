ad7293a0c4024d0065087fbef191dc467b9bca7d
mrsherpacode <sherpacoding@gmail.com>
2025-07-17 21:24:07 -0500

    created functions that changes the positon of the map and get its position lat and lng.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,19 +1,34 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { useState } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
+import { useEffect, useState } from "react";
+import { useCities } from "./contexts/CitiesContext";
 //  map component
 function Map() {
-  // Programmatic navigation allows us to go to other pages without us having to click on navigation link.
-  // const naviate = useNavigate();
   // This will get lat and lang from url and we use useSearchParams hook  for that
-  // const [searchParam, setSearchParam] = useSearchParams();
+  const [searchParam, setSearchParam] = useSearchParams();
   // here i'm using get method to get the position lat and lng cuz we directly can not access it.
-  // const lat = searchParam.get("lat");
-  // const lng = searchParam.get("lng");
-  ////////////////////////////////////////////////////////////////////////////////////////////////
+  const mapLat = searchParam.get("lat");
+  const mapLng = searchParam.get("lng");
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+  // useCities function is from CityContext file
+  const { cities } = useCities();
   //  mapPosition is for position of the map that usually take the lat and lng
-  const [mapPostion, setMapPosition] = useState([40, 0]);
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+  ///////////////////////////////////////////////////////
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng]
+  );
   return (
     <div className={styles.mapContainer}>
       {/* This container is from open-source React components for Leaflet maps (https://react-leaflet.js.org/) 
@@ -21,7 +36,7 @@ function Map() {
        to use this go to site and look for instructions .
       */}
       <MapContainer
-        center={mapPostion}
+        center={mapPosition}
         zoom={13}
         scrollWheelZoom={true}
         className={styles.map}
@@ -30,14 +45,42 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPostion}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+        {/* This is custom component that gets the postion of the map based on city */}
+        <ChangeCenter position={mapPosition} />
+        {/* when we click on the map it renders  the form and gets its lat and lang from url  */}
+        <DetectClick />
       </MapContainer>
     </div>
   );
+}
+
+///////////////////////////////////////
+// This function changes the position of the map based on it's cities.
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+///////////////////////////////////////
+/// This function gets  the position of  lat and lng when we click on the map.
+function DetectClick() {
+  // Programmatic navigation allows us to go to other pages without us having to click on navigation link.
+  const navigate = useNavigate();
+  useMapEvents({
+    // when click on map it renders form and gets it's lat and lng from url
+    click: (e) => navigate(`form?lat=${e.latlng.lat}?lng${e.latlng.lng}`),
+  });
 }
 
 export default Map;
