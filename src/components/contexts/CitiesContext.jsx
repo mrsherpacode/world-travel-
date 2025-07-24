4de74025328098c36233fa53d699ce2fd346636a
mrsherpacode <sherpacoding@gmail.com>
2025-07-24 14:48:39 -0500

    i coverted all the useState hook to useReducer hook for better logic

--- src/components/contexts/CitiesContext.jsx
+++ src/components/contexts/CitiesContext.jsx
@@ -1,27 +1,87 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 // creating a context
 const CitiesContext = createContext();
 // API URL //
 const BASE_URL = "http://localhost:9000";
+
+//  initial states
+const initialStates = {
+  cities: [],
+  isLoading: false,
+  currentCity: {},
+  error: "",
+};
+//  Reducer function
+function reducer(state, action) {
+  switch (action.type) {
+    case "loading":
+      return { ...state, isLoading: true };
+    // here i'm using common naming convention
+    //  when all the cities are  loaded
+    case "cities/loaded":
+      return {
+        ...state,
+        isLoading: false,
+        cities: action.payload,
+      };
+    // When a single city is loaded
+    case "city/loaded":
+      return {
+        ...state,
+        isLoading: false,
+        currentCity: action.payload,
+      };
+    // when a single city is created
+    case "city/created":
+      return {
+        ...state,
+        isLoading: false,
+        cities: [...state.cities, action.payload],
+        currentCity: action.payload,
+      };
+    // when a single city is deleted
+    case "city/deleted":
+      return {
+        ...state,
+        isLoading: false,
+        cities: state.cities.filter((city) => city.id !== action.payload),
+        currentCity: {},
+      };
+    // when there is error
+
+    case "rejected":
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      };
+
+    default:
+      "unknown action  type";
+  }
+}
+
 // This is a context provider
 function CitiesProvider({ children }) {
-  // we are creating cities state here cuz we need this state in other places too.
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [currentCity, setCurrentCity] = useState({});
+  // here i'm destructuring initial states.
+  const [{ cities, isLoading, currentCity, error }, dispatch] = useReducer(
+    reducer,
+    initialStates
+  );
   // This hook loads cities data from  the fake API that i created
   useEffect(
     function () {
       async function fetchCities() {
-        setIsLoading(true);
+        dispatch({ type: "loading" });
         try {
           const res = await fetch(`${BASE_URL}/cities`);
           const data = await res.json();
-          setCities(data);
+          dispatch({ type: "cities/loaded", payload: data });
         } catch (err) {
-          console.error(err);
-        } finally {
-          setIsLoading(false);
+          dispatch({
+            type: "rejected",
+            payload: "There was error loading cities ...",
+          });
         }
       }
       fetchCities();
@@ -31,21 +91,23 @@ function CitiesProvider({ children }) {
   );
   //  this function is called in the City component on its initial render.
   async function getCity(id) {
-    setIsLoading(true);
+    if (Number(id) === currentCity.id) return;
+    dispatch({ type: "loading" });
     try {
       const res = await fetch(`${BASE_URL}/cities/${id}`);
       const data = await res.json();
-      setCurrentCity(data);
+      dispatch({ type: "city/loaded", payload: data });
     } catch (err) {
-      console.error(err);
-    } finally {
-      setIsLoading(false);
+      dispatch({
+        type: "rejected",
+        payload: "There was error loading city ...",
+      });
     }
   }
 
   // This function uploads newCity object to the fake API that i created before  //
   async function createCity(newCity) {
-    setIsLoading(true);
+    dispatch({ type: "loading" });
     try {
       const res = await fetch(`${BASE_URL}/cities`, {
         method: "POST",
@@ -58,28 +120,30 @@ function CitiesProvider({ children }) {
       // console.log(data);
       // This synches UI with the remote data, the one that comes from the API
       // it adds newCity data to current cities array object
-      setCities((cities) => [...cities, data]);
+      dispatch({ type: "city/created", payload: data });
     } catch (err) {
-      console.error(err);
-    } finally {
-      setIsLoading(false);
+      dispatch({
+        type: "rejected",
+        payload: "There was error creating newCity ...",
+      });
     }
   }
 
   /////////////////////////////////////////
   // This function delete the city list from the  cities
   async function deleteCity(id) {
-    setIsLoading(true);
+    dispatch({ type: "loading" });
     try {
       await fetch(`${BASE_URL}/cities/${id}`, {
         method: "DELETE",
       });
-      // it creates a new city without the city that has the matching id and cities list will re-render without the deleted city.
-      setCities((cities) => cities.filter((city) => city.id !== id));
+      // it creates a new city array without the city that has the matching id and cities list will re-render without the deleted city.
+      dispatch({ type: "city/deleted", payload: id });
     } catch (err) {
-      console.error(err);
-    } finally {
-      setIsLoading(false);
+      dispatch({
+        type: "rejected",
+        payload: "There was error deleting city ...",
+      });
     }
   }
 
@@ -92,6 +156,7 @@ function CitiesProvider({ children }) {
         currentCity,
         createCity,
         deleteCity,
+        error,
       }}
     >
       {children}
---
+++ src/components/contexts/UseCitiesContext.jsx
@@ -0,0 +1,111 @@
+//  This is using useState() hook, which i created before creating useReducer() hook.
+// just for the record
+
+import { createContext, useContext, useEffect, useState } from "react";
+// creating a context
+const CitiesContext = createContext();
+// API URL //
+const BASE_URL = "http://localhost:9000";
+// This is a context provider
+function CitiesProvider({ children }) {
+  // we are creating cities state here cuz we need this state in other places too.
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
+  // This hook loads cities data from  the fake API that i created
+  useEffect(
+    function () {
+      async function fetchCities() {
+        setIsLoading(true);
+        try {
+          const res = await fetch(`${BASE_URL}/cities`);
+          const data = await res.json();
+          setCities(data);
+        } catch (err) {
+          console.error(err);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+      fetchCities();
+    },
+
+    []
+  );
+  //  this function is called in the City component on its initial render.
+  async function getCity(id) {
+    setIsLoading(true);
+    try {
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  // This function uploads newCity object to the fake API that i created before  //
+  async function createCity(newCity) {
+    setIsLoading(true);
+    try {
+      const res = await fetch(`${BASE_URL}/cities`, {
+        method: "POST",
+        body: JSON.stringify(newCity),
+        headers: {
+          "content-type": "application/json",
+        },
+      });
+      const data = await res.json();
+      // console.log(data);
+      // This synches UI with the remote data, the one that comes from the API
+      // it adds newCity data to current cities array object
+      setCities((cities) => [...cities, data]);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  /////////////////////////////////////////
+  // This function delete the city list from the  cities
+  async function deleteCity(id) {
+    setIsLoading(true);
+    try {
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE",
+      });
+      // it creates a new city without the city that has the matching id and cities list will re-render without the deleted city.
+      setCities((cities) => cities.filter((city) => city.id !== id));
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+        getCity,
+        currentCity,
+        createCity,
+        deleteCity,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+// here I'm creating a custom hook for consuming citiesContext values
+function useCities() {
+  const value = useContext(CitiesContext);
+  if (value === undefined)
+    throw new Error("CitiesContext is used outside the CitiesProvider");
+  return value;
+}
+export { CitiesProvider, useCities };
